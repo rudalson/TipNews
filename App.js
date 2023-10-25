b4b12196bd8674459870a67bfee0bebd107062f8
rudalson <rudalson@gmail.com>
2023-10-25 19:01:02 +0900

    modify : fix the safe area except status bar

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 import HomeNavigator from './App/Routes/HomeNavigator';
 
 export default function App() {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-        marginHorizontal: 15,
-      }}
-    >
-      <NavigationContainer>
-        <HomeNavigator />
-      </NavigationContainer>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          marginHorizontal: 15,
+        }}
+      >
+        <NavigationContainer>
+          <HomeNavigator />
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
